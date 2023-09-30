393264256a6de3afad42cd143ad316092b8fef96
Adekunle Adedeji <ceeepath@gmail.com>
2023-09-30 18:39:02 +0000

    Terraform installation script created

---
+++ bin/install_terraform.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+# Ensure that apt package repository is up to date and install gnupg & software-properties-common.
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+# Install the HashiCorp GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# Verify the key's fingerprint
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+# Add the official HashiCorp repository to your system
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+# Download the package information from HashiCorp and Install Terraform from the new repository
+sudo apt update && sudo apt-get install terraform
+# verify the terraform installation
+terraform -help
