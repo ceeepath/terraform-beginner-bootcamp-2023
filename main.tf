376dfe586d60117c2c6c6c7a2cc229d6f08b4ab4
Adekunle Adedeji <ceeepath@gmail.com>
2023-11-06 17:01:27 +0000

    confirm binary created - custom-terraform-provider

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
+}
+
 # terraform {
 
 #   # cloud {
@@ -19,29 +28,35 @@
 #   # Configuration options
 # }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  # Creating the S3 Bucket
-  user_uuid   = var.user_uuid
-  bucket_name = var.bucket_name
-
-  # Naming documents for static website hosting
-  website_files = {
-    index = var.website_files.index
-    error = var.website_files.error
-  }
-
-  # Configuring the S3 Bucket for static website hosting
-  file_path = {
-    index = var.file_path.index
-    error = var.file_path.error
-  }
-
-  # Implementing content versioning for website files
-  content_version = var.content_version
-
-  # Uploading Assets to s3 bucket
-  assets_path = var.assets_path
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
+
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   # Creating the S3 Bucket
+#   user_uuid   = var.user_uuid
+#   bucket_name = var.bucket_name
+
+#   # Naming documents for static website hosting
+#   website_files = {
+#     index = var.website_files.index
+#     error = var.website_files.error
+#   }
+
+#   # Configuring the S3 Bucket for static website hosting
+#   file_path = {
+#     index = var.file_path.index
+#     error = var.file_path.error
+#   }
+
+#   # Implementing content versioning for website files
+#   content_version = var.content_version
+
+#   # Uploading Assets to s3 bucket
+#   assets_path = var.assets_path
+# }
 
 
